f91459fec4969411be54b38e98e6772fc6b41261
Manognabandi21 <manognabandi21@gmail.com>
2025-08-26 16:45:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await axios.post('http://localhost:8081/insert', product);
+    await axios.post('http://localhost:9090/springapp1/insert', product);
     alert('Insert success');
     fetchProducts();
   };
 
   const fetchProducts = async () => {
-    const res = await axios.get('http://localhost:8081/display');
+    const res = await axios.get('http://localhost:9090/springapp1/display');
     setProducts(res.data);
   };
 
@@ -37,21 +37,51 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>ID:</label>
-          <input type="number" name="id" className="form-control" value={product.id} onChange={handleChange} required />
+          <input
+            type="number"
+            name="id"
+            className="form-control"
+            value={product.id}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>Name:</label>
-          <input type="text" name="name" className="form-control" value={product.name} onChange={handleChange} required />
+          <input
+            type="text"
+            name="name"
+            className="form-control"
+            value={product.name}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>OS:</label>
-          <input type="text" name="os" className="form-control" value={product.os} onChange={handleChange} required />
+          <input
+            type="text"
+            name="os"
+            className="form-control"
+            value={product.os}
+            onChange={handleChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label>Price:</label>
-          <input type="text" name="price" className="form-control" value={product.price} onChange={handleChange} required />
+          <input
+            type="text"
+            name="price"
+            className="form-control"
+            value={product.price}
+            onChange={handleChange}
+            required
+          />
         </div>
-        <button type="submit" className="btn btn-primary mt-2">Insert</button>
+        <button type="submit" className="btn btn-primary mt-2">
+          Insert
+        </button>
       </form>
 
       <h3 className="mt-5">Product List</h3>
